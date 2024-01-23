f8f55306b0659b7f6952983550e0a11a3a0c00f3
voorukondan <voorukondan@gmail.com>
2024-01-23 13:26:22 +0000

    solution

---
+++ src/main/java/com/example/school/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+// Write your code here
+package com.example.school.repository;
+
+import com.example.school.model.Student;
+import java.util.*;
+
+public interface StudentRepository {
+ArrayList<Student> getStudents();
+Student getStudentById(int studentId);
+Student addStudent(Student student);
+String addMultipleStudents(ArrayList<Student> studentsList);
+Student updatStudent(int studentId, Student student);
+
+void deleteStudent(int studentId);
+}
